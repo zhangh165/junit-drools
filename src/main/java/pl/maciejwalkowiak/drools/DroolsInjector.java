4156abf6fa8ca1bc4e02cbd5e6382e00b7da352a
zhangh165 <dsa4444446@qq.com>
2022-07-01 16:09:09 +0800

    update dsl support,clean code

--- src/main/java/pl/maciejwalkowiak/drools/DroolsInjector.java
+++ src/main/java/pl/maciejwalkowiak/drools/DroolsInjector.java
@@ -2,14 +2,23 @@ package pl.maciejwalkowiak.drools;
 
 import org.drools.compiler.builder.impl.KnowledgeBuilderImpl;
 import org.drools.compiler.compiler.DroolsError;
+import org.drools.compiler.compiler.DroolsParserException;
 import org.drools.compiler.compiler.PackageBuilderErrors;
+import org.drools.core.io.impl.FileSystemResource;
+import org.drools.core.util.StringUtils;
+import org.kie.api.io.Resource;
 import org.kie.api.runtime.KieSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pl.maciejwalkowiak.drools.annotations.DroolsFiles;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 /**
@@ -19,6 +28,7 @@ import java.util.Arrays;
  */
 public class DroolsInjector {
     private static final Logger LOG = LoggerFactory.getLogger(DroolsInjector.class);
+    private final KnowledgeBuilderImpl builder = new KnowledgeBuilderImpl();
 
     public void initDrools(Object testClass) throws Exception {
         if (testClass == null) {
@@ -31,21 +41,59 @@ public class DroolsInjector {
         DroolsFiles droolsFiles = annotationProcessor.getDroolsFiles();
 
         DroolsSession droolsSession = 
-          initKnowledgeBase(droolsFiles.location(), Arrays.asList(droolsFiles.value()));
+          initKnowledgeBase(droolsFiles.location(),
+                  droolsFiles.dsl(),
+                  Arrays.asList(droolsFiles.value()));
 
         annotationProcessor.setDroolsSession(droolsSession);
     }
 
     private DroolsSession initKnowledgeBase(String droolsLocation,
-                                            Iterable<String> fileNames) throws Exception {
+                                            String dslFileName,
+                                            Iterable<String> drlFileNames) throws Exception {
 
-        KnowledgeBuilderImpl builder = new KnowledgeBuilderImpl();
+        LOG.info("Initializing knowledge base for drl files located in {} with names: {}", droolsLocation, drlFileNames);
 
-        LOG.info("Initializing knowledge base for drl files located in {} with names: {}", droolsLocation, fileNames);
-        for (String fileName : fileNames) {
+        loadRuleFiles(droolsLocation,dslFileName,drlFileNames);
+
+        checkErrorsExistsInKnowledgeBase();
+
+        KieSession session = builder.newKieBase().newKieSession();
+
+        return new DroolsSessionImpl(session);
+    }
+
+    private void loadRuleFiles(String droolsLocation,
+                               String dslFileName,
+                               Iterable<String> drlFileNames) throws IOException, DroolsParserException {
+        for (String fileName : drlFileNames) {
             builder.addPackageFromDrl(loadDroolFile(droolsLocation, fileName));
         }
 
+        if(!StringUtils.isEmpty(dslFileName)){
+            builder.addPackageFromDslr(loadDslFile(droolsLocation,dslFileName));
+        }
+    }
+
+    private InputStreamReader loadDroolFile(String droolsLocation, String filename) {
+        InputStream stream = getClass().getResourceAsStream(droolsLocation + filename);
+
+        if (stream == null) {
+            throw new IllegalArgumentException("File not found in location: " + droolsLocation + filename);
+        }
+        return new InputStreamReader(stream);
+    }
+
+    private Resource loadDslFile(String droolLocation,String fileName){
+        Path dslFilePath = Paths.get(droolLocation+File.pathSeparator+fileName);
+        if(Files.exists(dslFilePath)){
+            throw new IllegalArgumentException("File not found in location: "+dslFilePath);
+        }
+
+        return new FileSystemResource(dslFilePath.toString());
+    }
+
+    private void checkErrorsExistsInKnowledgeBase(){
         PackageBuilderErrors errors = builder.getErrors();
 
         // Make sure that there are no errors in knowledge base
@@ -58,18 +106,5 @@ public class DroolsInjector {
 
             throw new IllegalStateException("There are errors in DRL files");
         }
-
-        KieSession session = builder.newKieBase().newKieSession();
-
-        return new DroolsSessionImpl(session);
-    }
-
-    private InputStreamReader loadDroolFile(String droolsLocation, String filename) {
-        InputStream stream = getClass().getResourceAsStream(droolsLocation + filename);
-
-        if (stream == null) {
-            throw new IllegalArgumentException("File not found in location: " + droolsLocation + filename);
-        }
-        return new InputStreamReader(stream);
     }
 }
